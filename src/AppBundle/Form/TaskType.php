40b640785d047b133236640ceff56161e5107694
RSHKDL <julien.helfer@gmail.com>
2020-07-09 17:34:37 +0200

    add missing translation in TaskType

--- src/AppBundle/Form/TaskType.php
+++ src/AppBundle/Form/TaskType.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
@@ -12,11 +13,15 @@ class TaskType extends AbstractType
      * @param FormBuilderInterface $builder
      * @param array $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('content', TextareaType::class)
+            ->add('title', TextType::class, [
+                'label' => 'form.task.title'
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'form.task.content'
+            ])
         ;
     }
 }
